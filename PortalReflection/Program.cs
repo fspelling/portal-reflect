ac0d3b5b73e3c98e68051b3c0f49513ce6bbec8f
Fernando Spelling <f.l.spelling@gmail.com>
2020-05-19 10:05:02 -0300

    initial

---
+++ PortalReflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortalReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
